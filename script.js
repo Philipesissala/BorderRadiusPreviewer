523364b84ef097057156eb4ba8b6ee7e8d4dfa4a
Filipe Sissala <filipesissala14@gmail.com>
2021-02-20 12:53:21 +0100

    chore: Adicionando a funcionalidade de gerar css

--- script.js
+++ script.js
@@ -17,12 +17,13 @@ const App = {
         }
     },
     init() {
-        const { inputUp, inputRight, inputLeft, inputDown } = App.getValues();
+        let { inputUp, inputRight, inputLeft, inputDown } = App.getValues();
         App.box.style.borderTopLeftRadius = `${inputUp}px`;
         App.box.style.borderTopRightRadius = `${inputRight}px`;
         App.box.style.borderBottomLeftRadius = `${inputLeft}px`;
         App.box.style.borderBottomRightRadius = `${inputDown}px`;
 
+        App.inputGenerateCss.value = `border-radius: ${inputUp.trim() == 0 ? 0 : inputUp}px ${inputRight.trim() == 0 ? 0 : inputRight}px ${inputLeft.trim() == 0 ? 0 : inputLeft}px ${inputDown.trim() == 0 ? 0 : inputDown}px;`;
     },
 }
 App.btnApply.addEventListener("click", () => {
